cbe08a78e33aafbdcbe320327d44e3e5afe1b3fd
MikhailR1337 <mihail.rogov@mail.ru>
2021-04-05 16:41:59 +0300

    compelte App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Navbar } from './components/NavBar';
+import { AboutPage } from './pages/AboutPage';
+import { TodosPage } from './pages/TodosPage';
+
 
 const App: React.FC = () =>  {
   return (
-    <h1>Hello TS</h1>
+    <BrowserRouter>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route component={TodosPage} path="/" exact/>
+          <Route component={AboutPage} path="/about" />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+interface TodoFormProps {
+    onAdd(title: string): void
+}
+
+export const TodoForm: React.FC<TodoFormProps> = (props) => {
+    const [title, setTitle] = useState<string>('');
+
+    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.target.value);
+    }
+
+    const onKeyHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            props.onAdd(title);
+            setTitle('');
+        }
+    }
+
+    return (
+        <div className="input-field mt2">
+            <input
+            onKeyPress={onKeyHandler}
+            onChange={changeHandler}
+            value={title}
+            type="text" id="title"
+            placeholder="Введите название дела"/>
+            <label htmlFor="title" className="active">
+                Введите название дела
+            </label>
+        </div>
+    );
+}
